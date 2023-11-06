4be0a7ca50c29287d146d7da984bd7af6cf686d1
tonoy3125 <shaifshajedt@gmail.com>
2023-11-07 00:37:26 +0600

    send the data using post method

--- src/Pages/AddJob/JobDetail.jsx
+++ src/Pages/AddJob/JobDetail.jsx
@@ -1,12 +1,43 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from './../../Providers/Authprovider';
+import toast, { Toaster } from "react-hot-toast";
 
 
 const JobDetail = ({ job }) => {
-    const { _id, name, category_name, deadline, minimum_price, maximum_price, description } = job
+    const { user } = useContext(AuthContext)
+    const { _id, name, category_name, deadline, email, minimum_price, maximum_price, description } = job
 
-    
+    const handleBidJob = (event) => {
+        event.preventDefault()
+        const form = event.target
+        const useremail = form.useremail.value
+        const buyeremail = form.buyeremail.value
+        const deadline = form.deadline.value
+        const price = form.price.value
+        const newBidJob = { useremail, buyeremail, deadline, price }
+        console.log(newBidJob)
+
+        fetch("http://localhost:5000/bid", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(newBidJob),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                toast.success("Your product has been added to the cart.");
+            });
+
+
+
+
+    }
+
+
 
     return (
         <div className="mt-10 container mx-auto">
@@ -30,34 +61,35 @@ const JobDetail = ({ job }) => {
                     </div>
                 </div>
                 <div>
-                    <form className="mt-10">
+                    <form onSubmit={handleBidJob} className="mt-10">
                         <div className="flex flex-col md:flex-row items-center gap-6 mb-6 px-1 ">
                             <div className="w-full md:w-1/2">
                                 <h2 className=" text-base md:text-xl font-semibold text-[#1B1A1ACC] mb-4">User Email <span className="text-red-700">*</span></h2>
-                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff]  border border-black text-base font-normal text-[#1B1A1A99] rounded" type="email" name="email" placeholder="Enter Email Address Here" id="" />
+                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff]  border border-black text-base font-normal text-[#1B1A1A99] rounded" type="email" name="useremail" defaultValue={user?.email} readOnly placeholder="Enter Email Address Here" id="" />
                             </div>
                             <div className="w-full md:w-1/2">
                                 <h2 className=" text-base md:text-xl font-semibold text-[#1B1A1ACC] mb-4">Buyer Email <span className="text-red-700">*</span></h2>
-                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff] border border-black text-base font-normal text-[#1B1A1A99] rounded" type="email" name="email" placeholder="Enter Email Address Here" id="" />
+                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff] border border-black text-base font-normal text-[#1B1A1A99] rounded" type="email" name="buyeremail" defaultValue={email} readOnly placeholder="Enter Email Address Here" id="" />
                             </div>
                         </div>
                         <div className="flex flex-col md:flex-row items-center gap-6 mb-6gap-6 mb-6 px-1  ">
                             <div className="w-full md:w-1/2">
                                 <h2 className=" text-base md:text-xl font-semibold text-[#1B1A1ACC] mb-4">Deadline <span className="text-red-700">*</span></h2>
-                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff] border border-black text-base font-normal text-[#1B1A1A99] rounded" type="date" name="deadline" value={deadline}  placeholder="Deadline" id="" />
+                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff] border border-black text-base font-normal text-[#1B1A1A99] rounded" type="date" name="deadline" value={deadline} placeholder="Deadline" id="" />
                             </div>
                             <div className="w-full md:w-1/2">
                                 <h2 className=" text-base md:text-xl font-semibold text-[#1B1A1ACC] mb-4">Price <span className="text-red-700">*</span></h2>
-                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff] border border-black text-base font-normal text-[#1B1A1A99] rounded" type="text" name="maximum_price" placeholder="Maximum Price" id="" />
+                                <input className="pt-5 pb-5 pl-2 md:p-5 w-full bg-[#fff] border border-black text-base font-normal text-[#1B1A1A99] rounded" type="text" name="price" placeholder="Price" id="" />
                             </div>
                         </div>
 
                         <div className="px-24 pb-10">
-                            <input type="submit" value="Add Job" className="btn btn-block bg-[#196F3D] text-lg text-[#fff] hover:bg-[#154360] " />
+                            <input type="submit" value="Bid on the project" className="btn btn-block bg-[#196F3D] text-lg text-[#fff] hover:bg-[#154360] " />
                         </div>
                     </form>
                 </div>
             </div>
+            <Toaster></Toaster>
         </div>
     );
 };
