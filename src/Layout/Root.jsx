91d6c0e79e98dfd29709a14430ec8fc2f75182d9
tonoy3125 <shaifshajedt@gmail.com>
2023-11-08 18:03:42 +0600

    fevicon and title added commit

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -5,7 +5,7 @@ import Footer from "../Pages/Footer/Footer";
 
 const Root = () => {
     return (
-        <div >
+        <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
